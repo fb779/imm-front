5d8ff378021a92278ce24180c96b627da538d610
Fabian Forero <fabianforero779@gmail.com>
2020-07-10 08:07:27 -0400

    Ajuste visibilidad de boton miembros de familia.
    Se realiza ajuste de validacion para la visibilidad de botones de interaccion con miembros de la familia.

--- src/app/components/family/family.component.ts
+++ src/app/components/family/family.component.ts
@@ -1,69 +1,88 @@
-import { Component, OnInit, Input  } from '@angular/core';
-import { NbDialogService } from '@nebular/theme';
-import { Process } from '../../models/Process';
-import { FormFamilyMembersComponent } from './form-family-members/form-family-members.component';
-import { DeleteFamilyMemberComponent } from './delete-family-member/delete-family-member.component';
-import { FamilyService, ToastrService } from '../../services/services.index';
-import { status } from '../../config/config';
-import { of } from 'rxjs';
+import { Component, OnInit, Input } from "@angular/core";
+import { NbDialogService } from "@nebular/theme";
+import { Process } from "../../models/Process";
+import { FormFamilyMembersComponent } from "./form-family-members/form-family-members.component";
+import { DeleteFamilyMemberComponent } from "./delete-family-member/delete-family-member.component";
+import { FamilyService, ToastrService } from "../../services/services.index";
+import { status } from "../../config/config";
+import { of } from "rxjs";
 
 @Component({
-  selector: 'ngx-family',
-  templateUrl: './family.component.html',
-  styleUrls: ['./family.component.scss']
+  selector: "ngx-family",
+  templateUrl: "./family.component.html",
+  styleUrls: ["./family.component.scss"],
 })
 export class FamilyComponent implements OnInit {
-
   @Input() process: Process;
-  @Input() url:string[];
+  @Input() url: string[];
 
   visible: boolean = true; // define la visibilidad de los botones de editar y eliminar
 
   listFamilyMembers$ = this._familyServices.listFamilyMembers$;
-  // numberFamilyMembers$ = this._familyServices.numberFamilyMembers$;
-  numberFamilyMembers = this._familyServices.numberFamilyMembers;
+  numberFamilyMembers: number;
 
-  constructor(private _toastr: ToastrService, private dialogService: NbDialogService, private _familyServices: FamilyService) { }
+  constructor(
+    private _toastr: ToastrService,
+    private dialogService: NbDialogService,
+    private _familyServices: FamilyService
+  ) {}
 
   ngOnInit() {
-    // console.log('Proceso cargado en componente de miembros de familia',this.process);
+    console.log(
+      "Proceso cargado en componente de miembros de familia",
+      this.process
+    );
+    console.log("url cargada", this.url);
     this._familyServices.chageProcess(this.process._id);
+    this._familyServices.numberFamilyMembers$.subscribe((response) => {
+      this.numberFamilyMembers = response;
+      this.isVisible();
+    });
   }
 
   // isVisible( url: string[], process: Process, NumberFamily: number  ){
-  isVisible(){
-    let visible = false;
+  isVisible() {
+    this.visible = false;
 
-    if ( this.url[0] === 'pages'){
-      if ( this.process.status === status.form && this.numberFamilyMembers < (this.process.companion+1) ){
-        visible = true;
+    if (this.url[0] === "pages") {
+      // console.log("miembros de la familia cargados", this.numberFamilyMembers);
+      // console.log("acompañantes permitidos", this.process.companion);
+      if (
+        this.process.status === status.active
+        // && this.numberFamilyMembers < parseInt(this.process.companion + 1)
+      ) {
+        this.visible = true;
+        console.log("entramos al caso de verdad", this.visible);
       }
     } else {
-      if ( this.process.status !== status.form){
-        visible = true;
+      if (this.process.status !== status.form) {
+        this.visible = true;
       }
     }
-    return of(visible);
   }
 
-  openDialog(){
-    if ( this.process.companion + 1 <= this._familyServices.numberFamilyMembers ){
-        this._toastr.toastrGenericMessage('Limit of family members', 'Family Members', 'warning');
-        return;
+  openDialog() {
+    if (parseInt(this.process.companion + 1) <= this.numberFamilyMembers) {
+      this._toastr.toastrGenericMessage(
+        "Limit of family members",
+        "Family Members",
+        "warning"
+      );
+      return;
     }
 
     this.dialogService.open(FormFamilyMembersComponent, {
-      context:{
+      context: {
         process: this.process,
-        url: this.url
+        url: this.url,
       },
     });
   }
 
-  updateClient(ev){
+  updateClient(ev) {
     // console.log('actualizar cliente',ev);
     this.dialogService.open(FormFamilyMembersComponent, {
-      context:{
+      context: {
         process: this.process,
         url: this.url,
         client: ev,
@@ -71,17 +90,24 @@ export class FamilyComponent implements OnInit {
     });
   }
 
-  deleteClient(ev){
+  deleteClient(ev) {
     // console.log('eliminar cliente',ev);
-    this.dialogService.open( DeleteFamilyMemberComponent, {
-      context: { client: ev }
-    }).onClose.subscribe( res =>{
-        if (res){
-          this._familyServices.removeFamiliMember(this.process, ev ).subscribe(() => {
-            this._toastr.toastrGenericMessage('Remove family member', 'Family members', 'success');
-          });
+    this.dialogService
+      .open(DeleteFamilyMemberComponent, {
+        context: { client: ev },
+      })
+      .onClose.subscribe((res) => {
+        if (res) {
+          this._familyServices
+            .removeFamiliMember(this.process, ev)
+            .subscribe(() => {
+              this._toastr.toastrGenericMessage(
+                "Remove family member",
+                "Family members",
+                "success"
+              );
+            });
         }
       });
   }
-
 }
