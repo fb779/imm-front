35c11d09ab9556872c666d384d278397adfe1986
Fabian Forero <fabianforero779@gmail.com>
2020-09-18 15:49:18 -0400

    Ajuste imagenes en dashboard cliente
    Adicion de imagenes y ajuste en las imagenes del dashboard del cliente

--- src/app/pages/dashboard/cards/cards.component.ts
+++ src/app/pages/dashboard/cards/cards.component.ts
@@ -9,53 +9,60 @@ import { Router } from "@angular/router";
 export class CardsComponent implements OnInit {
   data: any = [
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/account.svg`,
       sub_title: `Settings`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `primary`,
-      link: "/pages/profile",
+      link: "/pages/settings",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/assessment-form.svg`,
       sub_title: `Assessment-Form`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `info`,
       link: "/pages/assessment-form",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/forms-guides.svg`,
       sub_title: `Forms & Guides`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `warning`,
       link: "/pages/forms-guides",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/documents.svg`,
       sub_title: `My Documents`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `danger`,
       link: "/pages/documents",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/family.svg`,
       sub_title: `My Family`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `primary`,
       link: "/pages/family",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/consultant.svg`,
       sub_title: `My Consultant`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `info`,
       link: "/pages/my-consultant",
     },
     {
-      img: `assets/images/card-user-profile.svg`,
+      img: `assets/images/messages.svg`,
       sub_title: `Messages`,
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
       accent: `warning`,
       link: "/pages/messages",
+    },
+    {
+      img: `assets/images/add-ons.svg`,
+      sub_title: `Add-ons`,
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,facere.`,
+      accent: `danger`,
+      link: "/pages/add-ons",
     },
   ];
 
